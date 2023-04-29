ecdc53285b2f4de9dbd1c18dd3b73ce1cc723ef6
ikonkobo <ikonkobo.idrissa@gmail.com>
2023-04-29 09:20:07 +0000

    density plots

--- src/models/bg_nbd.py
+++ src/models/bg_nbd.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import plotly.express as px
+import plotly.graph_objects as go
 from pandas import DataFrame
 from pymc_marketing.clv import BetaGeoModel
 from pymc import HalfNormal
@@ -37,24 +37,32 @@ class BetaGeoModel(BetaGeoModel):
             r_prior=HalfNormal.dist(10),
         )
 
-    def probability_alive(
+    def probability_alive_features(
             self,
             T_: int,
-            customer_history: DataFrame) -> Tuple[Any]:
-        alive_proba_xarray = self.expected_probability_alive(
+            customer_history: DataFrame
+            ) -> Tuple[float, float, float, Any]:
+        p_alive_xarray = self.expected_probability_alive(
             customer_id=customer_history[RawFeatures.CUSTOMER_ID],
             frequency=customer_history[RawFeatures.frequency],
             recency=customer_history[RawFeatures.recency],
             T=customer_history[RawFeatures.T]
         )
-        alive_proba_study_time = alive_proba_xarray.median(
+        min_p_alive_ = p_alive_xarray.to_numpy().min(axis=0).min(axis=0)
+        max_p_alive_ = p_alive_xarray.to_numpy().max(axis=0).max(axis=0)
+        alive_proba_study_time = p_alive_xarray.median(
                                     ("draw", "chain")
                                 ).to_numpy()[
                                     customer_history[
                                         customer_history["T"] == T_
                                     ].index.values[0]
                                 ]
-        return alive_proba_study_time, alive_proba_xarray
+        return (
+            alive_proba_study_time,
+            min_p_alive_,
+            max_p_alive_,
+            p_alive_xarray
+        )
 
     def plot_probability_alive(
             self,
@@ -72,25 +80,43 @@ class BetaGeoModel(BetaGeoModel):
                                                 customer_id,
                                                 n_period
                                             )
-        _, p_alive = self.probability_alive(
-            T_,
-            customer_history
-        )
-        """az.plot_hdi(
-                customer_history[RawFeatures.T],
-                p_alive,
-                color="C0"
-        )"""
-        fig = px.line(
-            x=customer_history[RawFeatures.T],
-            y=p_alive.median(("draw", "chain")),
+        (
+            alive_proba_study_time,
+            min_p_alive_,
+            max_p_alive_,
+            p_alive_xarray
+        ) = self.probability_alive_features(
+                T_,
+                customer_history
+            )
+
+        fig = go.Figure([
+                go.Scatter(
+                    x=customer_history[RawFeatures.T],
+                    y=p_alive_xarray.median(("draw", "chain")),
+                    width=4,
+                    marker=True,
+                ),
+                go.Scatter(
+                    x=list(
+                        customer_history[RawFeatures.T]
+                      )+list(
+                            customer_history[RawFeatures.T][::-1]
+                        ),
+                    y=list(max_p_alive_)+list(min_p_alive_[::-1]),
+                    fill='toself',
+                    hoverinfo='skip',
+                    showlegend=False,
+                )
+            ])
+        fig.update_layout(
+            xaxis_title="T",
+            yaxis_title="probability",
+            title=f"Probability Customer {customer_id} will purchase again",
+            hovermode='x',
             width=fig_dim[0],
             height=fig_dim[1],
-            markers=True,
-            line_shape='linear',
-            title=f"Probability Customer {customer_id} will purchase again"
-            )
-        fig.update_traces(patch={"line_shape": "spline"})
+        )
         fig.add_vline(
             x=T_,
             line_width=3,
@@ -113,10 +139,6 @@ class BetaGeoModel(BetaGeoModel):
                 line_color="black",
                 annotation_text="Purchase"
             )
-        fig.update_layout(
-            xaxis_title="T",
-            yaxis_title="probability"
-        )
         fig.show()
 
     def global_plots_(self):
